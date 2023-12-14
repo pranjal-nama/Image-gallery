96d6da9bb55296f2a97b263689cdd1c1f2ed75a7
Pranjal Nama <pranjalnama01@gmail.com>
2023-12-14 22:45:12 +0530

    fix image url in Image modal

--- src/components/imageModal/imageModal.jsx
+++ src/components/imageModal/imageModal.jsx
@@ -1,12 +1,16 @@
-import { Dialog, DialogTitle, DialogContent, Typography, CardMedia } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, CardMedia } from "@mui/material";
 
 const ImageModal = ({ open, handleClose, image }) => {
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>{image?.alt}</DialogTitle>
+      <DialogTitle>{image?.alt_description}</DialogTitle>
       <DialogContent>
-        <CardMedia component="img" image={image?.url} alt={image?.alt} style={{ maxWidth: '100%', height: 'auto' }} />
-        <Typography>Display image details here...</Typography>
+        <CardMedia 
+            component="img" 
+            image={image?.links.download} 
+            alt={image?.alt_description} 
+            style={{ maxWidth: '100%', height: 'auto' }} 
+        />
       </DialogContent>
     </Dialog>
   );
