00631eefbd7acf9738d4ad7ff6d03800d3153a07
Pranjal Nama <pranjalnama01@gmail.com>
2023-12-19 15:37:45 +0530

    Add css to search bar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,41 @@
-import { AppBar, Toolbar, Typography, CardMedia, CardContent, Box} from "@mui/material";
+import { AppBar, Toolbar, Typography, CardMedia, CardContent, Box, Switch, Grid} from "@mui/material";
+import { useState } from "react";
 import SearchBar from "../searchBar/searchBar";
 import './header.css';
 
 const Header = ({ handleSearch }) => {
+    const [darkMode, setDarkMode] = useState(false);
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        // Example: document.body.classList.toggle('dark-mode');
+    };
 
 	return (
         <>
 		<AppBar position="static" sx={{ backgroundColor: '#ffffff' }}> 
-            <Toolbar sx={{ justifyContent: 'center' }}>
+            <Toolbar sx={{ justifyContent: "space-between" }}>
                 <Typography
                     variant="h5"
                     component="div"
-                    sx={{ flexGrow: 1 }}
-					color="common.black"
+                    sx={{ flex: 1, whiteSpace: 'nowrap' }}
+                    color="textPrimary"
                 >
                     Image Gallery
                 </Typography>
-                <SearchBar handleSearch={handleSearch}/>
+                <Grid container justifyContent="center" alignItems="center">
+                    <Grid item>
+                        <SearchBar handleSearch={handleSearch} />
+                    </Grid>            
+                </Grid>
+                <Grid item sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography 
+                        sx={{ flex: 1, whiteSpace: 'nowrap' }}
+                        color="textSecondary"
+                    >
+                        Dark Mode
+                    </Typography>
+                    <Switch checked={darkMode} onChange={toggleDarkMode} />
+                </Grid>
             </Toolbar>
         </AppBar>
         <Box position="relative">
@@ -24,11 +43,12 @@ const Header = ({ handleSearch }) => {
                 component="img"
                 image="src\assets\mountain.jpg"
                 alt="header_cover"
-                style={{ objectFit: 'cover', height: "22rem" }}
+                style={{ objectFit: 'cover', height: "15rem" }}
             />
             <CardContent className="cover-text">
                 <Typography variant="h6">Download high-quality images by creators</Typography>
                 <Typography variant="body1">Over 2.4 million+ stock images by our talented community</Typography>
+                <SearchBar handleSearch={handleSearch}/>
             </CardContent>
         </Box>
         </>
